9ca4f1926c020ff9b1c20a1806e2f91af99c18f4
Racconeee <raccon6614@gmail.com>
2023-07-28 15:35:48 +0900

    Raccon

---
+++ src/main/java/RaccoonWorld/Raccoon/RaccoonApplication.java
@@ -0,0 +1,13 @@
+package RaccoonWorld.Raccoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaccoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaccoonApplication.class, args);
+	}
+
+}
